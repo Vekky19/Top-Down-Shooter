8774017a3ef7b008dcec0b54bb65edd41470f8ee
msievertLTU <60272260+msievertLTU@users.noreply.github.com>
2020-02-04 01:05:55 -0500

    Updated Project

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float playerSpeed = 3;
+    public Transform player;
+    public Quaternion rotation;
+    public float ylock;
+
+    void Start()
+    {
+        player = GetComponent<Transform>();
+        ylock = player.position.y;
+        rotation = player.rotation;
+    }
+
+    void MovePlayer()
+    {
+        float xMovement = Input.GetAxis("Horizontal") * Time.deltaTime;
+
+        if (xMovement != 0)
+        {
+            player.position = new Vector2(player.position.x + xMovement * playerSpeed, ylock);
+        }
+    }
+
+    void Update()
+    {
+        MovePlayer();
+        player.rotation = rotation;
+    }
+}
